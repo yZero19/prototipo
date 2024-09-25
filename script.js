db3d40d50e83eb81272f9b6934a7b98597e35e9a
Matag-E <mateussantos212@gmail.com>
2024-09-25 14:28:24 -0300

    correção script

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
+async function consultarCep(cepId, logradouroId, bairroId) {
+  const cepInput = document.getElementById(cepId);
+  const logradouroInput = document.getElementById(logradouroId);
+  const bairroInput = document.getElementById(bairroId);
+  const cep = cepInput.value.replace(/\D/g, '');
 
-async function consultarCep() {
-    const cepInput = document.getElementById('cep');
-    const enderecoInput = document.getElementById('endereco');
-    const cep = cepInput.value.replace(/\D/g, ''); 
-  
-    if (cep.length === 8) { 
-      try {
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        if (!response.ok) {
-          throw new Error('Erro na resposta da API');
-        }
-        
-        const data = await response.json();
-        console.log('Dados do CEP:', data); 
-  
-        if (data.erro) {
-          alert('CEP não encontrado');
-          enderecoInput.value = '';
-        } else {
-          enderecoInput.value = `${data.logradouro}, ${data.bairro} - ${data.localidade} - ${data.uf}`;
-        }
-      } catch (error) {
-        console.error('Erro ao consultar o CEP:', error);
-        alert('Erro ao consultar o CEP');
+  if (cep.length === 8) {
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      if (!response.ok) {
+        throw new Error('Erro na resposta da API');
       }
-    } else {
-      enderecoInput.value = '';
-    }
-  }
-  
 
-  document.getElementById('cep').addEventListener('blur', consultarCep);
+      const data = await response.json();
+      console.log('Dados do CEP:', data);
+
+      if (data.erro) {
+        alert('CEP não encontrado');
+        logradouroInput.value = '';
+        bairroInput.value = '';
+      } else {
+        logradouroInput.value = data.logradouro;
+        bairroInput.value = data.bairro;
+      }
+    } catch (error) {
+      console.error('Erro ao consultar o CEP:', error);
+      alert('Erro ao consultar o CEP');
+    }
+  } else {
+    logradouroInput.value = '';
+    bairroInput.value = '';
+  }
+}
+
+document.getElementById('cepEmissao').addEventListener('blur', () => {
+  consultarCep('cepEmissao', 'logradouroEmissao', 'bairroEmissao');
+});
+
+document.getElementById('cepEntrega').addEventListener('blur', () => {
+  consultarCep('cepEntrega', 'logradouroEntrega', 'bairroEntrega');
+});
